e9554a6c06d58207f7c8f50ef002701a2e32fbd5
Kushal S <185885530+kushal200514@users.noreply.github.com>
2026-08-22 09:12:34 +0530

    Commit existing project files

---
+++ pythonprac/Evenodd.py
@@ -0,0 +1,13 @@
+x = 11
+#check the remainder dividinf x by 2 is 0
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#checking another number
+x = 9
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
